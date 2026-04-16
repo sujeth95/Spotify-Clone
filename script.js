ac031790249bc4f47760d6bb40d29236e5c3b904
sujeth95 <sujeetoraon337@gmail.com>
2026-04-16 21:40:18 +0530

    Writing JS

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 console.log("Lets write javascript");
+
+const getSongs = async () => {
+    let a = await fetch("http://127.0.0.1:5500/songs/");
+    let response = await a.text();
+    // console.log(response);
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split("/songs/")[1]);
+        }
+    }
+
+    return songs;
+}
+
+const main = async () => {
+    let songs = await getSongs();
+    console.log(songs)
+
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li> ${song.replace("%20", " ")} </li>`;
+        
+    }
+
+    // Play the first song
+    var audio = new Audio(songs[0]);
+    // audio.play();
+
+    audio.addEventListener('loadeddata', () => {
+        console.log(audio.duration, audio.currentSrc, audio.currentTime);
+        // The duration variable now holds the duration (in seconds) of the audio clip
+    }); 
+}
+
+main();
